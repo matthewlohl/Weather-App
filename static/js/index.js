e50af270dbb7d84cd719cdfa177fe6df3b207cbb
Matthew Lo <matthewlol316@gmail.com>
2022-08-22 09:40:59 +0100

    create folders

---
+++ static/js/index.js
@@ -0,0 +1,1 @@
+let weather = {"apiKey": "574be141045fd202821891433c4208b7"}
---
+++ test/app.test.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+const path = require ('path')
+const app = require('../static/js/index.js')
+
+
