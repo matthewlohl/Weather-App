2084c63dd85f06f7de9bd08342b776834ccf0586
Matthew Lo <matthewlol316@gmail.com>
2022-10-23 17:51:25 +0100

    add search bar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Weather />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import Weather from './components/Weather.vue'
 
 export default {
-  name: 'App',
+  name: 'LayoutDefault',
+
   components: {
-    HelloWorld
+    Weather
+  },
+
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <section>
+      <div>
+        <Search />
+      </div>
+      <h1>Weather App</h1>
+    </section>
+  </main>
+</template>
+
+<style>
+  main{
+    display: flex;
+    margin-top: 10rem;
+    align-items: center;
+    justify-content: center;
+  }
+  section{
+
+    width: 50%;
+    border: 3px solid blue;
+  }
+</style>
+
+<script>
+import Search from'./Search'
+export default {
+  name: 'Weather-app',
+  components:{
+    Search
+  }
+}
+</script>
